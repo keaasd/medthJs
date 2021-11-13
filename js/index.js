13c54fe752d8d728bb8768cd06f1e763480fd80b
Коротин Евгений <kea-asd@yandex.ru>
2021-11-13 22:57:44 +0700

    js

--- js/index.js
+++ js/index.js
@@ -1,44 +1,77 @@
+// скрол блокируем
+const disabledScroll = () => {
+
+    const widthScroll = window.innerWidth - document.body.offsetWidth;
+    document.body.scrollPosition = window.scrollY;
+
+    document.body.style.cssText = `
+            overflow: hidden;
+            position: fixed;
+            top: -${document.body.scrollPosition}px;
+            left: 0;
+            height: 100vw;
+            width: 100vw;
+            padding-right: ${widthScroll}px;
+        `;
+}
+    const enabledScroll = () => {
+        document.body.style.cssText = 'position: relative';
+        window.scroll({top: document.body.scrollPosition})
+    
+}
+
+
 { // Модальное окно
     const presentOrderBtn = document.querySelector(
     '.present__order-btn');
     const pageOverlayModal = document.querySelector('.page__overlay_modal');
     const modalClose = document.querySelector('.modal__close');
 
-    const handlerModal = (openBtn, modal, openSelector, closeTrigger, sk = 'medium') => {
-        
-        let opacity = 0;
-        
+    const handlerModal = (openBtn, modal, openSelector, closeTrigger, sk = 'medium') => {        
+        let opacity = 0;        
         const speed = {
             slow: 15,
             medium: 8,
             fast: 1,
         };
-
-        openBtn.addEventListener('click', () => {
-            modal.style.opacity = opacity;
-            
+        const openModal = () => {
+            disabledScroll();
+            modal.style.opacity = opacity;            
             modal.classList.add(openSelector);
 
             const timer = setInterval (() => {
                 opacity += 0.02;
                 modal.style.opacity = opacity;
                 if (opacity >= 1) clearInterval(timer)
-            }, speed[sk] )        
-        });
-
-        closeTrigger.addEventListener('click', () => {
-
-            const timer = setInterval (() => {
-                opacity -= 0.02;
-                modal.style.opacity;
-                if (opacity <= 0) {
-                    clearInterval(timer)
-                    modal.classList.remove(openSelector)
-                }
             }, speed[sk])
+        };
 
-            
-        })
+
+
+
+    
+const closeModal = () => {
+    enabledScroll();
+    const timer = setInterval (() => {
+        opacity -= 0.02;
+        modal.style.opacity;
+        if (opacity <= 0) {
+            clearInterval(timer)
+            modal.classList.remove(openSelector);
+        }
+    }, speed[sk])            
+};
+
+openBtn.addEventListener('click', openModal);                 
+
+closeTrigger.addEventListener('click', closeModal);
+// клик мимо модального окна
+modal.addEventListener('click', () => {
+    if (event.target === modal) {
+        closeModal()
+    }
+    
+})
     };
 
     handlerModal(presentOrderBtn, 
