54eee47a055a45e792ba5972fb3fe7c3e3fedda4
Коротин Евгений <kea-asd@yandex.ru>
2021-11-13 17:14:58 +0700

    d

--- js/index.js
+++ js/index.js
@@ -1,49 +1,73 @@
-const presentOrderBtn = document.querySelector(
-'.present__order-btn');
-const pageOverlayModal = document.querySelector('.page__overlay_modal');
-const modalClose = document.querySelector('.modal__close');
+{ // Модальное окно
+    const presentOrderBtn = document.querySelector(
+    '.present__order-btn');
+    const pageOverlayModal = document.querySelector('.page__overlay_modal');
+    const modalClose = document.querySelector('.modal__close');
 
-const handlerModal = (openBtn, modal, openSelector, closeTrigger, sk = 'medium') => {
-    
-    let opacity = 0;
-    
-    const speed = {
-        slow: 15,
-        medium: 8,
-        fast: 1,
+    const handlerModal = (openBtn, modal, openSelector, closeTrigger, sk = 'medium') => {
+        
+        let opacity = 0;
+        
+        const speed = {
+            slow: 15,
+            medium: 8,
+            fast: 1,
+        };
+
+        openBtn.addEventListener('click', () => {
+            modal.style.opacity = opacity;
+            
+            modal.classList.add(openSelector);
+
+            const timer = setInterval (() => {
+                opacity += 0.02;
+                modal.style.opacity = opacity;
+                if (opacity >= 1) clearInterval(timer)
+            }, speed[sk] )        
+        });
+
+        closeTrigger.addEventListener('click', () => {
+
+            const timer = setInterval (() => {
+                opacity -= 0.02;
+                modal.style.opacity;
+                if (opacity <= 0) {
+                    clearInterval(timer)
+                    modal.classList.remove(openSelector)
+                }
+            }, speed[sk])
+
+            
+        })
     };
 
-    openBtn.addEventListener('click', () => {
-        modal.style.opacity = opacity;
-        
-        modal.classList.add(openSelector);
+    handlerModal(presentOrderBtn, 
+        pageOverlayModal, 
+        'page__overlay_modal_open', 
+        modalClose,
+        'slow'
+        );
+}
+{// Бургер меню
+    const headerContactsBurger = document.querySelector('.header__contacts-burger');
+    const headerContacts = document.querySelector('.header__contacts');
 
-        const timer = setInterval (() => {
-            opacity += 0.02;
-            modal.style.opacity = opacity;
-            if (opacity >= 1) clearInterval(timer)
-        }, speed[sk] )        
-    });
+    const handlerBurger = (openBtn, menu, openSelector) => {
+        openBtn.addEventListener('click', () => {
+            menu.style.height = '';            
+            if (menu.classList.contains(openSelector)) {
+                menu.classList.remove(openSelector);
+            } else {                
+                menu.style.height = menu.scrollHeight + 'px';
+                menu.classList.add(openSelector);
+            }            
+        })
+    };
 
-    closeTrigger.addEventListener('click', () => {
 
-        const timer = setInterval (() => {
-            opacity -= 0.02;
-            modal.style.opacity;
-            if (opacity <= 0) {
-                clearInterval(timer)
-                modal.classList.remove(openSelector)
-            }
-        }, speed[sk])
+    handlerBurger(headerContactsBurger, headerContacts, 'header__contacts_open')
 
-        
-    })
-};
 
-handlerModal(presentOrderBtn, 
-    pageOverlayModal, 
-    'page__overlay_modal_open', 
-    modalClose,
-    'slow'
-    );
-
+}
+// contains - метод, возвращает булевое значение
+// menu.classList.toggle(openSelector);
